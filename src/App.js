8c17ef5ac6278f444a19100f1f54c0a5e3e97190
Luke Steinmann <luke.steinmann@gmail.com>
2022-03-09 10:57:31 -0500

    Fix button functionality and completed state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from 'react'
 
 const App = () => {
+  // Phony Database
+  const todo = ['taskA', 'taskB', 'taskC']
   // Duration in milliseconds for different tasks
-  const pomodoro = 1500000
-  const shortBreak = 5000
-  // const shortBreak = 300000
-  const longBreak = 600000
+  const pomodoro = { name: 'pomodoro', duration: 1500000 }
+  const shortBreak = { name: 'shortBreak' , duration: 300000 }
+  const longBreak = { name: 'longBreak', duration: 600000}
   // State hooks
   const [currentTask, setCurrentTask] = useState(pomodoro)
-  const [time, setTime] = useState(currentTask)
+  const [time, setTime] = useState(currentTask.duration)
   const [start, setStart] = useState(false)
   const [completed, setCompleted] = useState(0)
 
   useEffect(() => {
     if (time === 0) {
-      setCompleted(prev => prev + 1)
       setStart(false)
     }
-  }, [time])
+    if (time === 0 && currentTask.name === 'pomodoro') {
+      setCompleted(prev => prev + 1)
+    }
+  }, [time, currentTask])
 
   useEffect(() => {
     let interval = null
@@ -38,9 +41,9 @@ const App = () => {
     <div className='App'>
       <h1>Timer</h1>
       <div>
-        <button onClick={() => {setStart(false); setTime(pomodoro); setCurrentTask(pomodoro);}}>Pomodoro</button>
-        <button onClick={() => {setStart(false); setTime(shortBreak); setCurrentTask(shortBreak);}}>Short Break</button>
-        <button onClick={() => {setStart(false); setTime(longBreak); setCurrentTask(longBreak);}}>Long Break</button>
+        <button onClick={() => {setStart(false); setTime(pomodoro.duration); setCurrentTask(pomodoro);}}>Pomodoro</button>
+        <button onClick={() => {setStart(false); setTime(shortBreak.duration); setCurrentTask(shortBreak);}}>Short Break</button>
+        <button onClick={() => {setStart(false); setTime(longBreak.duration); setCurrentTask(longBreak);}}>Long Break</button>
       </div>
       <h2>
         <span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
@@ -50,7 +53,7 @@ const App = () => {
       <div>
         <button onClick={() => setStart(true)}>Play</button>
         <button onClick={() => setStart(false)}>Pause</button>
-        <button onClick={() => {setTime(currentTask); setStart(false);}}>Reset</button>
+        <button onClick={() => {setTime(currentTask.duration); setStart(false);}}>Reset</button>
       </div>
       <div>
         {`Completed: ${completed}`}
