c10618ef16401adf5607104221c50f32e3eddbf5
Luke Steinmann <luke.steinmann@gmail.com>
2022-04-05 15:02:49 -0400

    timer stop and go appearance

--- src/components/StopAndGoButton.js
+++ src/components/StopAndGoButton.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { FaPlay, FaStop, FaPause } from 'react-icons/fa'
 
 // StopAndGoButton component for starting and stopping the timer
 const StopAndGoButton = ({ start, setStart }) => {
+  const [className, setClassName] = useState('button stopAndGoButton')
   const onClickStopAndGo = () => {
     setStart(!start)
   }
 
+  useEffect(() => {
+    start ? setClassName('button stopAndGoButton stopAndGoStopper') : setClassName('button stopAndGoButton')
+  }, [start])
+
   return (
-    <button onClick={onClickStopAndGo} className='button stopAndGoButton'>{start ? 'Stop' : 'Start'}</button>
+    <button onClick={onClickStopAndGo} className={className}>{start ? <FaPause/> : <FaPlay/>}</button>
   ) 
 }
 
